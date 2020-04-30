0ea69503d4f321f62ae22906df8a9963f2362f2f
Alex <alex_15_96@live.com.mx>
2020-04-30 00:25:23 -0500

    Finished

--- tictactoe.py
+++ tictactoe.py
@@ -1,43 +1,84 @@
-symbols = list(input("Enter cells:"))
+def winner(state, rows_):
+    columns = [[state[x] for x in range(i, 9, 3)] for i in range(3)]
+    diagonal_D = [state[x] for x in range(0, 9, 4)]
+    diagonal_I = [state[x] for x in range(2, 7, 2)]
+
+    space = True if state.count(" ") > 0 else False
+
+    o_win = False
+    x_win = False
+
+    # o_number = symbols.count("O")
+    # x_number = symbols.count("X")
+
+    x_win = True if diagonal_I.count("X") == 3 else x_win
+    x_win = True if diagonal_D.count("X") == 3 else x_win
+
+    o_win = True if diagonal_I.count("O") == 3 else o_win
+    o_win = True if diagonal_D.count("O") == 3 else o_win
+
+    if not (o_win or x_win):
+        for row in rows_:
+            if row.count("O") == 3:
+                o_win = True
+            elif row.count("X") == 3:
+                x_win = True
+
+        for col in columns:
+            if col.count("O") == 3:
+                o_win = True
+            elif col.count("X") == 3:
+                x_win = True
+
+    # if abs(o_number - x_number) >= 2 or (o_win and x_win):
+    # print("Impossible")
+    # return True
+    # elif not (o_win or x_win) and space:
+    # print("Game not finished")
+    if not (o_win or x_win) and not space:
+        print("Draw")
+        return True
+    elif o_win:
+        print("O wins")
+        return True
+    elif x_win:
+        print("X wins")
+        return True
+    else:
+        return False
+
+
+def print_game(matrix):
+    print("---------")
+    for row in matrix:
+        print("|", end=" ")
+        for element in row:
+            print(element + " ", end="")
+        print("|")
+    print("---------")
+
+
+symbols = list("         ")
 rows = [symbols[i:i + 3] for i in range(0, 9, 3)]
-columns = [[symbols[x] for x in range(i, 9, 3)] for i in range(3)]
 
-diagonal_D = [symbols[x] for x in range(0, 9, 4)]
-diagonal_I = [symbols[x] for x in range(2, 7, 2)]
-O_win = False
-X_win = False
-
-space = True if symbols.count("_") > 0 else False
-
-print("---------")
-for row in rows:
-    print("|", end=" ")
-    for element in row:
-        print(element + " ", end="")
-    print("|")
-print("---------")
+print_game(rows)
 
 rep = True
 input_ = True
-count = 0
+Game = False
+player = True
 
-while rep:
+while not Game:
     while input_:
         try:
             x, y = map(int, input("Enter the coordinates: ").split())
         except:
             print("You should enter numbers!")
-            count += 1
         else:
             if x > 3 or y > 3:
                 print("Coordinates should be from 1 to 3!")
             else:
                 input_ = False
-        if count >= 2:
-            break
-    if count >= 2:
-        break
-
     x -= 1
     if y == 3:
         y = 0
@@ -48,49 +89,18 @@ while rep:
 
     if rows[y][x] == "X" or rows[y][x] == "O":
         print("This cell is occupied! Choose another one!")
-        rep = True
     else:
-        rows[y][x] = "X"
-        print("---------")
-        for row in rows:
-            print("|", end=" ")
-            for element in row:
-                print(element + " ", end="")
-            print("|")
-        print("---------")
-        rep = False
+        if player:
+            rows[y][x] = "X"
+        else:
+            rows[y][x] = "O"
+        print_game(rows)
+        player = not player
     input_ = True
 
+    x = []
+    for n in rows:
+        x.append("".join(n))
+    y = "".join(x)
 
-# o_number = symbols.count("O")
-# x_number = symbols.count("X")
-#
-# X_win = True if diagonal_I.count("X") == 3 else X_win
-# X_win = True if diagonal_D.count("X") == 3 else X_win
-#
-# O_win = True if diagonal_I.count("O") == 3 else O_win
-# O_win = True if diagonal_D.count("O") == 3 else O_win
-#
-# if not(O_win or X_win):
-#     for row in rows:
-#         if row.count("O") == 3:
-#             O_win = True
-#         elif row.count("X") == 3:
-#             X_win = True
-#
-#     for col in columns:
-#         if col.count("O") == 3:
-#             O_win = True
-#         elif col.count("X") == 3:
-#             X_win = True
-#
-# if abs(o_number-x_number) >= 2 or (O_win and X_win):
-#     print("Impossible")
-# elif not(O_win or X_win) and space:
-#     print("Game not finished")
-# elif not(O_win or X_win) and not space:
-#     print("Draw")
-# elif O_win:
-#     print("O wins")
-# elif X_win:
-#     print("X wins")
+    Game = winner(y, rows)
